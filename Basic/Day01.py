60f7f6a5c8cd694e4a7cfe5f59dd02f56673b2be
Rishabh <iamrishabhnirmalkar@gmail.com>
2023-12-19 11:22:20 +0530

    Day01

---
+++ Basic/Day01.py
@@ -0,0 +1,17 @@
+
+# 1. Python Comments
+'''This is comment'''
+"""This is also comment"""
+
+# 2. Print statements
+print("hello word")  # hello world
+print(5)  # 5
+print(12*2)  # 24
+print("hey", 5, 65, sep="-")  # sep is seprator value
+print("hello", 45, 4545, end="009\n")
+# end is use to write last value in print statement adn use \n for next line otherwise stick with the next print statement
+
+
+# 3. Escape Sequence (\n or \""\) this is use for new line
+print("hello i am good boy \n and this viewer is also good boy")
+print("hey i am a \"good boy\"and this is viewer")
---
+++ README.md
@@ -0,0 +1,9 @@
+# Day 01
+
+1. Python Comments
+2. Print statements
+3. Escape Sequence
+
+# Day 02
+
+1.
